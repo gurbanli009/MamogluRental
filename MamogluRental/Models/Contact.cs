8ddd1c0e992e9da326f6699f42c6ec3e995276ec
gurbanli <gurbanli009@gmail.com>
2025-05-08 21:31:25 +0400

    commit

---
+++ MamogluRental/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MamogluRental.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gmail { get; set; }
+        public string Message { get; set; }
+
+        public int? UserId { get; set; } // Optional relationship
+        public User? User { get; set; }
+    }
+}
--- MamogluRental/Models/User.cs
+++ MamogluRental/Models/User.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
 
         public string Role { get; set; }
 
